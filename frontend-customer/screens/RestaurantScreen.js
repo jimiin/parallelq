67d1c5176f32f58e48afb483194fe64296d2625d
Jimin <jp4218@ic.ac.uk>
2020-06-02 18:08:22 +0100

    [jp4218] Change the header title according to the name of restaurant

--- frontend-customer/screens/RestaurantScreen.js
+++ frontend-customer/screens/RestaurantScreen.js
@@ -8,19 +8,20 @@ async function makeOrder() {
   const res = await axios.post(url + "/orders/add", {
     items: "Burger"
   });
-  console.log(`Status code: ${res.status}`);
-  console.log(`Status text: ${res.statusText}`);
-  console.log(`Request method: ${res.request.method}`);
-  console.log(`Path: ${res.request.path}`);
-  console.log(`Date: ${res.headers.date}`);
-  console.log(`Data: ${res.data}`);
-  // navigation.navigate('Home');
 }
 
 export default function RestaurantScreen({ navigation, route }) {
+  const title = route.params.title;
+
+  navigation.setOptions(
+    {
+      headerTitle: title
+    }
+  );
+
   return (
     <View>
-      <Text> This is {route.params.title} screen </Text>
+      <Text> This is {title} screen </Text>
       <Button
         title='Order'
         onPress={() => { makeOrder() }}
