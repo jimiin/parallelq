53aecbf6454d4edce59c1c6a205947a9520d836a
Samira Brunmayr <samira.brunmayr@hotmail.com>
2020-06-02 20:04:36 +0100

    [ssb5018, as15118] Began implementing get requests for preparing and prepared orders

--- frontend-restaurant/screens/order/PreparingScreen.js
+++ frontend-restaurant/screens/order/PreparingScreen.js
@@ -9,38 +9,42 @@ import {
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import Accordion from 'react-native-collapsible/Accordion';
+import axios from 'axios';
 
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default class PreparingScreen extends Component {
 
-state = {
-    data: [
-      {
-        title: "Non-Veg Biryanis",
-        content:
-          "Biryani also known as biriyani, biriani, birani or briyani, is a mixed rice dish with its origins among the Muslims of the Indian subcontinent. This dish is especially popular throughout the Indian subcontinent, as well as among the diaspora from the region. It is also prepared in other regions such as Iraqi Kurdistan."
-      },
-      {
-        title: "Pizzas",
-        content:
-          "Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and various other ingredients (anchovies, olives, meat, etc.) baked at a high temperature, traditionally in a wood-fired oven. In formal settings, like a restaurant, pizza is eaten with a knife and fork, but in casual settings, it is cut into wedges to be eaten while held in the hand. Small pizzas are sometimes called pizzettas."
-      },
-      {
-        title: "Drinks",
-        content:
-          "A drink (or beverage) is a liquid intended for human consumption. In addition to their basic function of satisfying thirst, drinks play important roles in human culture. Common types of drinks include plain drinking water, milk, coffee, tea, hot chocolate, juice, and soft drinks. In addition, alcoholic drinks such as wine, beer, and liquor, which contain the drug ethanol, have been part of human culture for more than 8,000 years."
-      },
-      {
-        title: "Deserts",
-        content:
-          'A dessert is typically the sweet course that concludes a meal in the culture of many countries, particularly Western culture. The course usually consists of sweet foods but may include other items. The word "dessert" originated from the French word desservir "to clear the table" and the negative of the Latin word service'
-      }
-    ],
-    activeSections: [0,1,2,3],
+state = { 
+    data: [],
+    activeSections: [],
     collapsed: false,
     multipleSelect: true,
   };
+
+  generateData = () => {
+    /* TODO: Retrieve all the preparing meals, 
+             convert them to JavaScript objects,
+             add elements to this.state.data */
+    
+    var newDataStartIndex = this.state.data.length;
+    const newData = [];
+    axios.get(`host.com:5000/orders/status/preparing`)
+      .then(res => {
+        var orders = res.map(jorder => JSON.parse(jorder))
+        for (let i = 0; i < orders.length; i++) {
+          newData.push({
+            id: orders[i].id,
+            items: orders[i].items
+          });
+        }
+        var diffLength = newData.length - newDataStartIndex - 1;
+        var toAddActiveSections = [...Array(diffLength).keys()].map(x => x + newDataStartIndex);
+        const newActiveSections = this.state.activeSections.concat(toAddActiveSections);
+        this.setState({data: this.state.data.concat(newData), activeSections: newActiveSections});
+      })
+    
+  }
 
 
   toggleExpanded = () => {
@@ -59,7 +63,7 @@ state = {
       <View style={[styles.row]}>
       <View style={{flexDirection:'column'}}>
 
-        <Text style={styles.title}>{section.title}</Text>
+        <Text style={styles.title}>{section.id}</Text>
 
         <View style={{flexDirection:'row'}}>
           <Text style={[styles.viewItems]}>{isActive ? 'Hide Items' : 'View Items'}</Text>
@@ -72,10 +76,6 @@ state = {
       </View>
       
     );
-  };
-
-  updateSections = activeSections => {
-    this.setState({ activeSections });
   };
 
   onHandleDelete = () => {
@@ -98,7 +98,7 @@ state = {
         transition="backgroundColor"
       >
         <Text>
-          {section.content}
+          {section.items}
         </Text>
       </Animatable.View>
     );
@@ -122,7 +122,7 @@ state = {
             duration={400}
             onChange={this.updateSections}
           />
-        </ScrollView>
+        </ScrollView> 
       </View>
     );
   }
--- frontend-restaurant/screens/order/ReadyScreen.js
+++ frontend-restaurant/screens/order/ReadyScreen.js
@@ -15,33 +15,36 @@ import Icon from "react-native-vector-icons/MaterialIcons";
 export default class ReadyScreen extends Component {
 
 state = {
-    data: [
-      {
-        title: "Non-Veg Biryanis",
-        content:
-          "Biryani also known as biriyani, biriani, birani or briyani, is a mixed rice dish with its origins among the Muslims of the Indian subcontinent. This dish is especially popular throughout the Indian subcontinent, as well as among the diaspora from the region. It is also prepared in other regions such as Iraqi Kurdistan."
-      },
-      {
-        title: "Pizzas",
-        content:
-          "Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and various other ingredients (anchovies, olives, meat, etc.) baked at a high temperature, traditionally in a wood-fired oven. In formal settings, like a restaurant, pizza is eaten with a knife and fork, but in casual settings, it is cut into wedges to be eaten while held in the hand. Small pizzas are sometimes called pizzettas."
-      },
-      {
-        title: "Drinks",
-        content:
-          "A drink (or beverage) is a liquid intended for human consumption. In addition to their basic function of satisfying thirst, drinks play important roles in human culture. Common types of drinks include plain drinking water, milk, coffee, tea, hot chocolate, juice, and soft drinks. In addition, alcoholic drinks such as wine, beer, and liquor, which contain the drug ethanol, have been part of human culture for more than 8,000 years."
-      },
-      {
-        title: "Deserts",
-        content:
-          'A dessert is typically the sweet course that concludes a meal in the culture of many countries, particularly Western culture. The course usually consists of sweet foods but may include other items. The word "dessert" originated from the French word desservir "to clear the table" and the negative of the Latin word service'
-      }
-    ],
+    data: [],
     activeSections: [],
     collapsed: false,
     multipleSelect: true,
   };
 
+
+  generateData = () => {
+    /* TODO: Retrieve all the preparing meals, 
+             convert them to JavaScript objects,
+             add elements to this.state.data */
+    
+    var newDataStartIndex = this.state.data.length;
+    const newData = [];
+    axios.get(`host.com:5000/orders/status/prepared`)
+      .then(res => {
+        var orders = res.map(jorder => JSON.parse(jorder))
+        for (let i = 0; i < orders.length; i++) {
+          newData.push({
+            id: orders[i].id,
+            items: orders[i].items
+          });
+        }
+        var diffLength = newData.length - newDataStartIndex - 1;
+        var toAddActiveSections = [...Array(diffLength).keys()].map(x => x + newDataStartIndex);
+        const newActiveSections = this.state.activeSections.concat(toAddActiveSections);
+        this.setState({data: this.state.data.concat(newData), activeSections: newActiveSections});
+      })
+    
+  }
 
   toggleExpanded = () => {
     this.setState({ collapsed: this.state.collapsed });
@@ -59,7 +62,7 @@ state = {
       <View style={[styles.row]}>
       <View style={{flexDirection:'column'}}>
 
-        <Text style={styles.title}>{section.title}</Text>
+        <Text style={styles.title}>{section.id}</Text>
 
         <View style={{flexDirection:'row'}}>
           <Text style={[styles.viewItems]}>{isActive ? 'Hide Items' : 'View Items'}</Text>
@@ -98,7 +101,7 @@ state = {
         transition="backgroundColor"
       >
         <Text>
-          {section.content}
+          {section.items}
         </Text>
       </Animatable.View>
     );
