4e9ef65fa5b9d4e1479dc1b122663d15912b3243
Jimin <jp4218@ic.ac.uk>
2020-06-04 13:33:38 +0100

    Add availability feature for customer side front end

--- frontend-customer/src/components/Menus.js
+++ frontend-customer/src/components/Menus.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 import { Image, ScrollView, View, Text, Button, TouchableOpacity } from "react-native";
+import Accordion from 'react-native-collapsible/Accordion';
 
 import { styles } from '../styles/styles';
 import { formatter } from '../styles/formatter';
-
-import * as Animatable from 'react-native-animatable';
-import Accordion from 'react-native-collapsible/Accordion';
-import axios from 'axios';
-
-import Icon from "react-native-vector-icons/MaterialIcons";
-
 
 class Menus extends Component {
 
@@ -30,30 +24,47 @@ class Menus extends Component {
     });
   };
 
+  renderItemButton = (item) => {
+    if (item.availability === "available") {
+      return (
+        <Button
+          title="Add to cart"
+          onPress={() => { this.props.onPress(item) }} />
+      )
+    }
+    return (
+      <Button
+        title="Unavailable"
+        disabled={true} />
+    )
+  }
+
   renderHeader = (section, _, isActive) => {
     return (
       <View>
         <View style={[styles.row]}>
-          <View style={{ flexDirection: 'column', flexShrink: 1}}>
+          <View style={{ flexDirection: 'column', flexShrink: 1 }}>
 
             <Text style={styles.title}>{section.name}</Text>
             <View style={styles.priceTag}>
-              <Text style={styles.itemPriceText}>{formatter.format(section.price)}</Text>
-              <Text style={(section.ordered > 0) ? { fontSize: 22 } : { color: 'transparent' }}>x{section.ordered}</Text>
+              <Text style={styles.itemPriceText}>
+                {formatter.format(section.price)}
+              </Text>
             </ View>
-            <View style={{width:500}} >
-              <Text style={isActive ? styles.description : { padding: 10 }}>  Description: {section.description.substring(0, 5)}... </Text>
-              <Text style={isActive ? { padding: 10 } : styles.description}>  Description: {section.description} </Text>
+            <View style={{ width: 500 }} >
+              <Text style={isActive ? styles.description : { padding: 10 }}>
+                Description: {section.description.substring(0, 5)}...
+              </Text>
+              <Text style={isActive ? { padding: 10 } : styles.description}>
+                Description: {section.description}
+              </Text>
             </View>
-
           </View>
 
           <View style={{ flexDirection: 'column', flexShrink: 1 }}>
-           {/* <Image style={styles.fitImage} source={require('../../assets/images/library.jpg')} /> */}
+            {/* <Image style={styles.fitImage} source={require('../../assets/images/library.jpg')} /> */}
             <Text style={{ height: 10 }} />
-            <Button title="Add to cart"
-              onPress={() => { this.props.onPress(section); section.ordered++; }} />
-
+            {this.renderItemButton(section)}
           </View>
         </View>
         <View style={isActive ? styles.inactive : styles.inactive}></View>
@@ -61,8 +72,6 @@ class Menus extends Component {
 
     );
   };
-
-
 
   renderContent(section, _, isActive) {
     return (<View></View>);
