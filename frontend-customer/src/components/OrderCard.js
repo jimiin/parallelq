b4fbdab0d2b21d1e1de565ef3ba079969ee270f7
Jimin <jp4218@ic.ac.uk>
2020-06-04 14:28:01 +0100

    Add the order in order card

--- frontend-customer/src/components/OrderCard.js
+++ frontend-customer/src/components/OrderCard.js
@@ -6,11 +6,22 @@ import { styles } from '../styles/styles';
 
 /* TODO: Add number of people in queue in case 2 */
 const orderStatus = (prepared, queuePosition) => {
-  switch(prepared) {
-  case 0: return (<Text style={styles.orderTitle}>Position in queue: {queuePosition + 1}</Text>)
-    case 1: return (<Text style={styles.orderTitle} >Pick Up Now</Text>)
+  switch (prepared) {
+    case 0:
+      return (
+        <Text style={styles.orderTitle}>
+          Position in queue: {queuePosition + 1}
+        </Text>
+      )
+    case 1:
+      return (
+        <Text style={styles.orderTitle}>
+          Pick Up Now
+        </Text>
+      )
     case 2:
-    default: return (<Text></Text>)
+    default:
+      return (<Text></Text>)
   }
 }
 
@@ -18,21 +29,25 @@ const orderTime = (creationTime) => {
   const dateTime = creationTime.split('T')
   const date = dateTime[0];
   const time = (dateTime[1]).split('.')[0];
-  return (<Text style={styles.optionText}>Ordered: {date + ' at ' + time}</Text>)
+  return (
+    <Text style={styles.optionText}>
+      Ordered: {date + ' at ' + time}
+    </Text>)
 }
 
 export default function OrderCard({ orderNumber, item, creationTime, prepared, queuePosition, onPress }) {
   return (
-    <RectButton style={[(prepared == 1) ? styles.preparedRow : styles.preparingRow, onPress = { onPress }, {borderRadius:10, margin:10}]} >
+    <RectButton style={[(prepared == 1) ? styles.preparedRow : styles.preparingRow, onPress = { onPress }, { borderRadius: 10, margin: 10 }]} >
       <View style={{ flexDirection: 'column' }}>
         <View style={styles.title}>
           <Text style={styles.orderTitle}>Order #{orderNumber}</Text>
+          <Text style={styles.itemNameText}>{item}</Text>
         </View>
         <View style={styles.optionTextContainer}>
           {orderTime(creationTime)}
         </View>
       </View>
-        {orderStatus(prepared, queuePosition)}
+      {orderStatus(prepared, queuePosition)}
     </ RectButton>
   );
 }
