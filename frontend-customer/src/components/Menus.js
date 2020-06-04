757090afa93dfa4389c9a65658b0ec8086dc5ce6
Jimin <jp4218@ic.ac.uk>
2020-06-04 14:27:45 +0100

    Fix a minor error in availability button

--- frontend-customer/src/components/Menus.js
+++ frontend-customer/src/components/Menus.js
@@ -35,7 +35,8 @@ class Menus extends Component {
     return (
       <Button
         title="Unavailable"
-        disabled={true} />
+        disabled={true}
+        onPress={() => { console.log("unavilable menu") }} />
     )
   }
 
