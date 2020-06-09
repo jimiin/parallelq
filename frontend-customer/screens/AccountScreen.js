c31ec5aa6efa13aa630e72405278a72869121ed5
Jimin <jp4218@ic.ac.uk>
2020-06-09 16:27:12 +0100

    Refactor account screen

--- frontend-customer/screens/AccountScreen.js
+++ frontend-customer/screens/AccountScreen.js
@@ -1,38 +1,22 @@
-import { Ionicons } from '@expo/vector-icons';
 import * as React from 'react';
+import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { styles } from '../src/styles/styles';
+import AccountProfile from '../src/components/AccountProfile';
 
 class AccountScreen extends React.Component {
   state = {}
   render() {
     return (
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
+      <View style={styles.container}>
         {
           (this.props.user) ? (
-            <View>
-              <View style={styles.profileImageContainer}>
-                <Image
-                  style={styles.profileImage}
-                  source={{ uri: this.props.user.photoUrl }} />
-              </View>
-
-              <View style={styles.nameContainer}>
-                <Text style={styles.nameText}>
-                  {this.props.user.name}
-                </Text>
-              </View>
-
-              <OptionButton
-                icon="md-mail"
-                label={this.props.user.email}
-              />
-            </View>
+            <AccountProfile
+              user={this.props.user}
+              signOut={this.props.signOut}
+              loadInitialPage={() => this.props.navigation.goBack(null)}
+            />
           ) : (
               <View style={{
                 flex: 1,
@@ -45,17 +29,7 @@ class AccountScreen extends React.Component {
               </View>
             )
         }
-
-        <OptionButton
-          icon="md-log-out"
-          label="Log out"
-          onPress={() => {
-            console.log("Log out")
-            this.props.signOut();
-            this.props.navigation.goBack(null);
-          }}
-        />
-      </ScrollView>
+      </View>
     );
   }
 }
@@ -68,74 +42,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    signIn: (user) => dispatch({ type: 'SIGN_IN', payload: user }),
     signOut: () => dispatch({ type: 'SIGN_OUT' }),
   }
 }
-
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton
-      style={[styles.option, isLastOption && styles.lastOption]}
-      onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={30} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-  },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 18,
-    alignSelf: 'flex-start',
-    marginTop: 1,
-  },
-  nameContainer: {
-    marginBottom: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  nameText: {
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-  profileImageContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  profileImage: {
-    margin: 10,
-    width: 100,
-    height: 100,
-    borderRadius: 50
-  }
-});
 
 export default connect(mapStateToProps, mapDispatchToProps)(AccountScreen)
---
+++ frontend-customer/src/components/AccountProfile.js
@@ -0,0 +1,110 @@
+import { Ionicons } from '@expo/vector-icons';
+import * as React from 'react';
+
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+
+class AccountProfile extends React.Component {
+  render() {
+    return (
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View style={styles.profileImageContainer}>
+          <Image
+            style={styles.profileImage}
+            source={{ uri: this.props.user.photoUrl }} />
+        </View>
+
+        <View style={styles.nameContainer}>
+          <Text style={styles.nameText}>
+            {this.props.user.name}
+          </Text>
+        </View>
+
+        <OptionButton
+          icon="md-mail"
+          label={this.props.user.email}
+        />
+
+        <OptionButton
+          icon="md-log-out"
+          label="Log out"
+          onPress={() => {
+            console.log("Log out")
+            this.props.signOut();
+            this.props.loadInitialPage();
+          }}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+function OptionButton({ icon, label, onPress, isLastOption }) {
+  return (
+    <RectButton
+      style={[styles.option, isLastOption && styles.lastOption]}
+      onPress={onPress}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.optionIconContainer}>
+          <Ionicons name={icon} size={30} color="rgba(0,0,0,0.35)" />
+        </View>
+        <View style={styles.optionTextContainer}>
+          <Text style={styles.optionText}>{label}</Text>
+        </View>
+      </View>
+    </RectButton>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  contentContainer: {
+    paddingTop: 15,
+  },
+  optionIconContainer: {
+    marginRight: 12,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 0,
+    borderColor: '#ededed',
+  },
+  lastOption: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  optionText: {
+    fontSize: 18,
+    alignSelf: 'flex-start',
+    marginTop: 1,
+  },
+  nameContainer: {
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nameText: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  profileImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileImage: {
+    margin: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  }
+});
+
+export default AccountProfile;
