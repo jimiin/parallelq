07bb7b5cfb39854b37d2197cd720d20a7a1995e5
Jimin <jp4218@ic.ac.uk>
2020-06-09 10:13:20 +0100

    Implement reducer for user log in info

--- frontend-customer/screens/LoginScreen.js
+++ frontend-customer/screens/LoginScreen.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
 import { View, Image, Button } from 'react-native';
+import { connect } from 'react-redux';
+import * as GoogleSignIn from 'expo-google-sign-in';
 
 import { styles } from '../src/styles/styles';
 
 class LoginScreen extends Component {
   state = {}
+
+  componentDidMount() {
+    this.initAsync();
+  }
+
+  initAsync = async () => {
+    await GoogleSignIn.initAsync({
+      clientId: '302399817797-0u74s0rbvcrmapn413h3fp703g98i91h.apps.googleusercontent.com',
+    });
+    this._syncUserWithStateAsync();
+  };
+
+  _syncUserWithStateAsync = async () => {
+    const user = await GoogleSignIn.signInSilentlyAsync();
+    // this.setState({ user });
+    this.props.signIn(user);
+  };
+
+  signOutAsync = async () => {
+    await GoogleSignIn.signOutAsync();
+    // this.setState({ user: null });
+    this.props.singOut();
+  };
+
+  signInAsync = async () => {
+    try {
+      await GoogleSignIn.askForPlayServicesAsync();
+      const { type, user } = await GoogleSignIn.signInAsync();
+      if (type === 'success') {
+        this._syncUserWithStateAsync();
+      }
+    } catch ({ message }) {
+      alert('login: Error:' + message);
+    }
+  };
+
+  onPress = () => {
+    if (this.props.user) {
+      this.signOutAsync();
+    } else {
+      this.signInAsync();
+    }
+  };
+
   render() {
     return (
       <View
@@ -15,10 +61,24 @@ class LoginScreen extends Component {
 
         <Button
           title="Sign in With Google"
-          onPress={() => this.props.navigation.navigate('Root')} />
+          onPress={this.onPress} />
       </View>
     );
   }
 }
 
-export default LoginScreen;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (user) => dispatch({ type: 'SIGN_IN', payload: user }),
+    singOut: () => dispatch({ type: 'SIGN_OUT' }),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen)
+//export default LoginScreen;
--- frontend-customer/src/store/index.js
+++ frontend-customer/src/store/index.js
@@ -2,10 +2,12 @@ import { combineReducers, createStore } from 'redux'
 
 import cartItems from '../reducers/cartItems'
 import favouriteItems from '../reducers/favouriteItems'
+import user from '../reducers/user'
 
 const rootReducer = combineReducers({
   cartItems: cartItems,
-  favItems: favouriteItems
+  favItems: favouriteItems,
+  user: user
 })
 
 export const store = createStore(rootReducer);
