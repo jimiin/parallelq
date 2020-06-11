161fb746de7fcc0b3c3a04707ba69faecd81ef7d
Ajanth Sutharsan <as15118@ic.ac.uk>
2020-06-11 14:02:49 +0100

    Redesigned log in screen

--- frontend-restaurant/screens/LoginScreen.js
+++ frontend-restaurant/screens/LoginScreen.js
@@ -6,6 +6,19 @@ class LoginScreen extends Component {
   state = {}
 
   render() {
+    let loginModel = t.struct({
+      restaurant_id: t.Number,
+    });
+
+    let options = {
+      fields: {
+        restaurant_id: {
+          label: 'Restaurant Id',
+          help: 'Please enter the id assigned to you',
+        },
+      },
+    };
+
     return (
       <View
         style={{
@@ -22,27 +35,49 @@ class LoginScreen extends Component {
           }}
           source={require('../assets/images/logo.png')} />
 
-        <Text>
-          Restaurant ID
-        </Text>
-
-        <TextInput
-          style={{ borderWidth: 1 }}
-          placeholder="Enter restaurant ID"
-          returnKeyLabel={"next"}
-          onChangeText={(text) => this.setState({ id: text })}
-        />
-
-        <Button
-          title="Sign in"
-          onPress={() => {
+        <View>
+          <Form
+            ref='form'
+            type={MenuItemModel}
+           options={{options}}
+          // value={{}}
+          // onChange={{}}
+          />
+          <TouchableOpacity style={styles.button} onPress={() => {
             console.log(this.state.id);
             this.props.signIn(this.state.id);
-          }} />
+          }} >
+             <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+      </View>
       </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+});
 
 const mapStateToProps = (state) => {
   return {
