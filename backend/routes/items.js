3aff9d95ef74c3ee592e5b1680bfe70acbb10c7b
Sei Aoki <seiya.aoki18@imperial.ac.uk>
2020-06-12 04:36:14 +0900

    Implement item edit and delete BE

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -41,22 +41,35 @@ router.route('/add').post((req, res) => {
     
 });
 
-router.route('/change_availability/:availability/:id').post((req, res) => {
-    const id = req.params.id;
-    const availability = req.params.availability;
+router.route('/delete/:id').delete((req, res) => {
+    const _id = req.params.id;
+    Item.findByIdAndDelete(_id)
+        .then(item => res.json('Deleted :'+item._id))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
-    Item.findById(id)
+router.route('/delete/:id').delete((req, res) => {
+    const _id = req.params.id;
+    Item.findByIdAndDelete(_id)
+        .then(item => res.json('Deleted :'+item._id))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/modify/:id').post((req, res) => {
+    const _id = req.params.id;
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    Item.findById(_id)
         .then(item => {
-            if (Object.values(AVAILABILITIES).includes(availability)) {
-                item.availability = availability;
+            item.name = name;
+            item.price = price;
+            item.description = description;
 
-                item.save()
-                    .then(() => res.json('Item availability updated'))
-                    .catch(err => res.status(400).json('Error: ' + err));
-            } else {
-                res.status(400).json('Error: Invalid availability')
-            }
-            
+            item.save()
+                .then(() => res.json('Item updated'))
+                .catch(err => res.status(400).json('Error: ' + err));
         })
         .catch(err => res.status(400).json('Error: ' + err));
 });
