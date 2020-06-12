79d7699c05e9122a4e299eebcaf7e06c9a23e87b
Jimin <jp4218@ic.ac.uk>
2020-06-12 05:16:15 +0100

    Edit styles for menu card for uniform button size

--- frontend-restaurant/components/MenuCard.js
+++ frontend-restaurant/components/MenuCard.js
@@ -1,94 +1,126 @@
-import * as React from 'react';
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import * as React from "react";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 
-
-import { styles } from '../styles/styles';
-import { formatter } from '../styles/formatter';
+import { styles } from "../styles/styles";
+import { formatter } from "../styles/formatter";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import axios from 'axios';
+import axios from "axios";
 
 class MenuCard extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       isActive: true,
       isAvailable: this.props.availability,
-    }
+    };
     this.changeText = this.changeText.bind(this);
     this.handlePress = this.handlePress.bind(this);
-
   }
-
-  
 
   changeText() {
     const previous = this.state.isActive;
-    this.setState({isActive: (!previous)});
+    this.setState({ isActive: !previous });
   }
 
-   /* Sets order to prepared and deletes from the list. */
+  /* Sets order to prepared and deletes from the list. */
   handlePress = async () => {
     const prev = this.state.isAvailable;
-    this.setState({isAvailable: (prev == "available" ? "unavailable" : "available")});
-   /* try {
-      console.log("about to do it");
-      var request = 'https://drp38-backend.herokuapp.com/items/change_availability/' + (this.props.availability == "available" ? "unavailable" : "available") + '/' + this.props.itemNumber;
-      console.log(request);
-      let res = await axios.post(request);
-    } catch (err) {
-      console.log(err)
-    } */
+    this.setState({
+      isAvailable: prev == "available" ? "unavailable" : "available",
+    });
+    // try {
+    //   console.log("about to do it");
+    //   var request =
+    //     "https://drp38-backend.herokuapp.com/items/change_availability/" +
+    //     (this.props.availability == "available" ? "unavailable" : "available") +
+    //     "/" +
+    //     this.props.itemNumber;
+    //   console.log(request);
+    //   let res = await axios.post(request);
+    // } catch (err) {
+    //   console.log(err);
+    // }
   };
 
   render() {
     return (
       <TouchableOpacity
         style={{ borderRadius: 10, margin: 10 }}
-        onPress={this.changeText} >
-         
-         <View>
-        <View style={[styles.row]}>
-          <View style={{ flexDirection: 'column' }}>
-
-            <Text style={styles.title}>{this.props.name}</Text>
-            <Text
-              style={this.state.isAvailable == "available" ? styles.available : styles.unavailable}>
-              {'Currently: ' + this.state.isAvailable}
-            </Text>
-            <Text>{'Item ID: ' + this.props.itemNumber}</Text>
-            <Text>{'Price: £' + this.props.price}</Text>
-
-            <View style={{ flexDirection: 'row' }}>
-              <Icon
-                name={this.state.isActive ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
-                size={30}
-                color={'black'} />
-            </View>
-
-          </View>
-
-          <View style={styles.rightContainer}>
-            <Button
-              title={"Make " + (this.state.isAvailable == "available" ? "unavailable" : "available")}
-              onPress={this.handlePress} />
-          </View>
-
-
-        </View>
-
+        onPress={this.changeText}
+      >
         <View>
-          <Text style={this.state.isActive ? [styles.content, styles.active]: {height:0}}>
-            {'Description: ' + this.props.description}
-          </Text>
-        </View>
-        <View style={styles.inactive}>
-        </View>
-        
-        <View style={this.state.isActive ? styles.active : styles.inactive}></View>
-      </View>
+          <View style={styles.column}>
+            <Text style={styles.title}>{this.props.name}</Text>
 
-      </ TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ flexDirection: "column" }}>
+                <Text
+                  style={
+                    this.state.isAvailable == "available"
+                      ? styles.available
+                      : styles.unavailable
+                  }
+                >
+                  {"Currently: " + this.state.isAvailable}
+                </Text>
+                <Text>{"Item ID: " + this.props.itemNumber}</Text>
+                <Text>{"Price: " + formatter.format(this.props.price)}</Text>
+
+                <Icon
+                  name={
+                    this.state.isActive
+                      ? "keyboard-arrow-up"
+                      : "keyboard-arrow-down"
+                  }
+                  size={30}
+                  color={"black"}
+                />
+              </View>
+
+              <View style={styles.rightContainer}>
+                <View style={{ flexDirection: "column", padding: 5 }}>
+                  <View style={{ padding: 2 }}>
+                    <Button
+                      title={
+                        "Make " +
+                        (this.state.isAvailable == "available"
+                          ? "unavailable"
+                          : "available")
+                      }
+                      onPress={this.handlePress}
+                    />
+                  </View>
+
+                  <View style={{ padding: 2 }}>
+                    <Button title="Edit" onPress={this.handlePress} />
+                  </View>
+
+                  <View style={{ padding: 2 }}>
+                    <Button title="Remove" onPress={this.handlePress} />
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View>
+            <Text
+              style={
+                this.state.isActive
+                  ? [styles.content, styles.active]
+                  : { height: 0 }
+              }
+            >
+              {"Description: " + this.props.description}
+            </Text>
+          </View>
+          <View style={styles.inactive}></View>
+
+          <View
+            style={this.state.isActive ? styles.active : styles.inactive}
+          ></View>
+        </View>
+      </TouchableOpacity>
     );
   }
 }
--- frontend-restaurant/styles/styles.js
+++ frontend-restaurant/styles/styles.js
@@ -1,88 +1,95 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions } from "react-native";
 
-const window = Dimensions.get('window');
+const window = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
-  
   preparingRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderColor: 'grey',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderColor: "grey",
     padding: 10,
     borderWidth: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   preparedRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderColor: 'grey',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderColor: "grey",
     padding: 10,
     borderWidth: 1,
-    backgroundColor: '#96cdff',
-  },container: {
+    backgroundColor: "#96cdff",
+  },
+  container: {
     flex: 1,
-    backgroundColor: '#f8f8f8',
+    backgroundColor: "#f8f8f8",
     padding: -15,
   },
   description: {
     height: 0,
-    flexWrap: 'wrap',
-    color: 'transparent',
+    flexWrap: "wrap",
+    color: "transparent",
   },
   title: {
-    textAlign: 'left',
+    textAlign: "left",
     fontSize: 22,
-    fontWeight: '300',
+    fontWeight: "300",
     marginBottom: 10,
   },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderColor: 'grey',
+  column: {
+    flexDirection: "column",
+    borderColor: "grey",
     padding: 10,
     borderRadius: 10,
     borderWidth: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: "grey",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: "white",
   },
   content: {
     padding: 20,
-    backgroundColor: '#fff',
-    borderColor: 'grey',
-    borderRadius: 2,
+    backgroundColor: "#fff",
+    borderColor: "grey",
+    borderRadius: 10,
     borderWidth: 1,
   },
   leftContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    justifyContent: "flex-start",
   },
   rightContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   active: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   inactive: {
-    backgroundColor: '#f8f8f8',
+    backgroundColor: "#f8f8f8",
     paddingBottom: 0,
   },
   available: {
-    textAlign: 'left',
+    textAlign: "left",
     fontSize: 22,
-    fontWeight: '300',
+    fontWeight: "300",
     marginBottom: 10,
-    color: '#29cf53',
+    color: "#29cf53",
   },
   unavailable: {
-    textAlign: 'left',
+    textAlign: "left",
     fontSize: 22,
-    fontWeight: '300',
+    fontWeight: "300",
     marginBottom: 10,
-    color: '#d93030',
+    color: "#d93030",
   },
-
 });
