37ac1418b200e91f5e1ec666ec8674fd6c025164
Jimin <jp4218@ic.ac.uk>
2020-06-13 14:04:43 +0100

    Add reducer for expo notification token

--- frontend-customer/src/store/index.js
+++ frontend-customer/src/store/index.js
@@ -1,13 +1,15 @@
-import { combineReducers, createStore } from 'redux'
+import { combineReducers, createStore } from "redux";
 
-import cartItems from '../reducers/cartItems'
-import favouriteItems from '../reducers/favouriteItems'
-import user from '../reducers/user'
+import cartItems from "../reducers/cartItems";
+import favouriteItems from "../reducers/favouriteItems";
+import user from "../reducers/user";
+import notificationToken from "../reducers/notificationToken";
 
 const rootReducer = combineReducers({
   cartItems: cartItems,
   favItems: favouriteItems,
-  user: user
-})
+  user: user,
+  token: notificationToken,
+});
 
 export const store = createStore(rootReducer);
