2388c9650dc264887d78b4ccf26098d63966831b
Sei Aoki <seiya.aoki18@imperial.ac.uk>
2020-06-14 19:36:26 +0100

    Add API call for restaurant list with queue sizes

--- backend/routes/restaurants.js
+++ backend/routes/restaurants.js
@@ -8,6 +8,22 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 })
 
+let Order = require('../models/order.model');
+
+router.route('/with_queue_size').get(async (req, res) => {
+    Restaurant.find()
+        .sort('_id')
+        .then(initialRestaurants => {
+            let restaurants = [];
+            initialRestaurants.forEach(e => restaurants.push(e.toObject()));
+            for (let i = 0; i < restaurants.length; i++) {
+                let orders = await Order.find({restaurant_id : restaurant[i]._id, status: 'preparing'});
+                restaurants[i].queue_size = orders.length;
+            }
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
 router.route('/:r_id').get((req, res) => {
     const restaurant_id = req.params.r_id;
     Restaurant.findById(restaurant_id)
