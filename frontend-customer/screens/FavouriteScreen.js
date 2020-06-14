06b7be570bcb04960f1ea4e8804097d6626fc302
Jimin <jp4218@ic.ac.uk>
2020-06-14 14:53:41 +0100

    Prevent items from different shopping cart to be in the shopping cart

--- frontend-customer/screens/FavouriteScreen.js
+++ frontend-customer/screens/FavouriteScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, Modal, TouchableWithoutFeedback } from "react-native";
+import { View, Text, Modal, Button, ToastAndroid } from "react-native";
 import { connect } from "react-redux";
 
 import MenuItems from "../components/MenuItems";
@@ -8,11 +8,26 @@ import { styles } from "../styles/styles";
 class FavouriteScreen extends React.Component {
   state = { modalVisible: false };
 
-  handleOpen = () => {
+  fromSameRestaurant(item) {
+    const itemCount = this.props.itemCount;
+
+    if (itemCount.length === 0) {
+      return true;
+    }
+    return itemCount[0].item.restaurant_id === item.restaurant_id;
+  }
+
+  addItemToCart(item) {
+    this.props.addItemToCart(item);
+    ToastAndroid.show("Item Added to Shopping Cart", ToastAndroid.SHORT);
+    this.props.navigation.navigate("Cart");
+  }
+
+  handleOpen = (item) => {
     this.setState({
       modalVisible: true,
+      item: item,
     });
-    setTimeout(this.handleClose, 1000);
   };
 
   handleClose = () => {
@@ -34,8 +49,11 @@ class FavouriteScreen extends React.Component {
             onPressFav={this.props.favItem}
             onPressUnfav={this.props.unfavItem}
             onPress={(item) => {
-              this.props.addItemToCart(item);
-              this.props.navigation.navigate("Cart");
+              if (this.fromSameRestaurant(item)) {
+                this.addItemToCart(item);
+              } else {
+                this.handleOpen(item);
+              }
             }}
           />
         ) : (
@@ -43,19 +61,41 @@ class FavouriteScreen extends React.Component {
             <Text style={styles.emptyText}>No favourite items</Text>
           </View>
         )}
+
         <Modal
           animationType="fade"
           transparent={true}
           visible={this.state.modalVisible}
           onRequestClose={this.handleClose}
         >
-          <TouchableWithoutFeedback onPress={this.handleClose}>
-            <View style={styles.notificationView}>
-              <View style={styles.notificationContainer}>
-                <Text style={{ color: "white" }}>Added to Shopping Cart!</Text>
+          <View style={styles.notificationView}>
+            <View style={styles.notificationContainer}>
+              <View style={{ padding: 10 }}>
+                <Text style={styles.notificationTitle}>
+                  You can only order items from one restaurant at a time
+                </Text>
+                <Text style={styles.notificationText}>
+                  Clear your shopping cart if you'd still like to order this
+                  item
+                </Text>
+              </View>
+
+              <View style={{ padding: 5 }}>
+                <Button
+                  title="Clear shopping cart and add"
+                  onPress={() => {
+                    this.props.resetCart();
+                    this.addItemToCart(this.state.item);
+                    this.handleClose();
+                  }}
+                />
+              </View>
+
+              <View style={{ padding: 5 }}>
+                <Button title="Cancel" onPress={() => this.handleClose()} />
               </View>
             </View>
-          </TouchableWithoutFeedback>
+          </View>
         </Modal>
       </View>
     );
@@ -71,6 +111,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    resetCart: () => dispatch({ type: "RESET" }),
     addItemToCart: (item) => dispatch({ type: "INC_ITEM", payload: item }),
     favItem: (item) => dispatch({ type: "FAV_ITEM", payload: item }),
     unfavItem: (item) => dispatch({ type: "UNFAV_ITEM", payload: item }),
--- frontend-customer/screens/RestaurantScreen.js
+++ frontend-customer/screens/RestaurantScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, ToastAndroid } from "react-native";
+import { View, Text, Modal, Button, ToastAndroid } from "react-native";
 import { connect } from "react-redux";
 import { Searchbar } from "react-native-paper";
 
@@ -8,7 +8,34 @@ import { styles } from "../styles/styles";
 import { axios, urlList } from "../constants/api";
 
 class RestaurantScreen extends React.Component {
-  state = { menu: [], searchQuery: "" };
+  state = { modalVisible: false, menu: [], searchQuery: "" };
+
+  fromSameRestaurant(item) {
+    const itemCount = this.props.itemCount;
+
+    if (itemCount.length === 0) {
+      return true;
+    }
+    return itemCount[0].item.restaurant_id === item.restaurant_id;
+  }
+
+  addItemToCart(item) {
+    this.props.addItemToCart(item);
+    ToastAndroid.show("Item Added to Shopping Cart", ToastAndroid.SHORT);
+  }
+
+  handleOpen = (item) => {
+    this.setState({
+      modalVisible: true,
+      item: item,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      modalVisible: false,
+    });
+  };
 
   compareMenu(a, b) {
     if (a.availability < b.availability) {
@@ -87,13 +114,49 @@ class RestaurantScreen extends React.Component {
           onPressFav={this.props.favItem}
           onPressUnfav={this.props.unfavItem}
           onPress={(item) => {
-            this.props.addItemToCart(item);
-            ToastAndroid.show(
-              "Item Added to Shopping Cart",
-              ToastAndroid.SHORT
-            );
+            if (this.fromSameRestaurant(item)) {
+              this.addItemToCart(item);
+            } else {
+              this.handleOpen(item);
+            }
           }}
         />
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={this.handleClose}
+        >
+          <View style={styles.notificationView}>
+            <View style={styles.notificationContainer}>
+              <View style={{ padding: 10 }}>
+                <Text style={styles.notificationTitle}>
+                  You can only order items from one restaurant at a time
+                </Text>
+                <Text style={styles.notificationText}>
+                  Clear your shopping cart if you'd still like to order this
+                  item
+                </Text>
+              </View>
+
+              <View style={{ padding: 5 }}>
+                <Button
+                  title="Clear shopping cart and add"
+                  onPress={() => {
+                    this.props.resetCart();
+                    this.addItemToCart(this.state.item);
+                    this.handleClose();
+                  }}
+                />
+              </View>
+
+              <View style={{ padding: 5 }}>
+                <Button title="Cancel" onPress={() => this.handleClose()} />
+              </View>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -108,6 +171,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    resetCart: () => dispatch({ type: "RESET" }),
     addItemToCart: (item) => dispatch({ type: "INC_ITEM", payload: item }),
     favItem: (item) => dispatch({ type: "FAV_ITEM", payload: item }),
     unfavItem: (item) => dispatch({ type: "UNFAV_ITEM", payload: item }),
