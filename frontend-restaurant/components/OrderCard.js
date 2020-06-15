31a04c8445a075e7ef9dd98024808cc6db47bdac
Jimin <jp4218@ic.ac.uk>
2020-06-15 09:24:27 +0100

    Update preparing  screen and order card to include special requirements

--- frontend-restaurant/components/OrderCard.js
+++ frontend-restaurant/components/OrderCard.js
@@ -1,116 +1,142 @@
-import * as React from 'react';
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import * as React from "react";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 
-
-import { styles } from '../styles/styles';
-import { formatter } from '../styles/formatter';
+import { styles } from "../styles/styles";
+import { formatter } from "../styles/formatter";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import axios from 'axios';
+import axios from "axios";
 
 class OrderCard extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       isActive: true,
-    }
+    };
     this.changeText = this.changeText.bind(this);
   }
 
-  
-
   changeText() {
     const previous = this.state.isActive;
-    this.setState({isActive: (!previous)});
+    this.setState({ isActive: !previous });
   }
-
-   /* Sets order to prepared and deletes from the list. */
-   onHandleReady = async () => {
+  /* Sets order to prepared and deletes from the list. */
+  onHandleReady = async () => {
     try {
-      console.log('got here')
-      await axios.post('https://drp38-backend.herokuapp.com/orders/change_status/prepared/' + this.props.id);
+      console.log("got here");
+      await axios.post(
+        "https://drp38-backend.herokuapp.com/orders/change_status/prepared/" +
+          this.props.order._id
+      );
       // Post notification
-      let restaurantName = (await axios.get('https://drp38-backend.herokuapp.com/restaurants/' + this.props.rid)).data.name;
+      let restaurantName = (
+        await axios.get(
+          "https://drp38-backend.herokuapp.com/restaurants/" +
+            this.props.order.user_id
+        )
+      ).data.name;
       console.log(restaurantName);
-      console.log(this.props.uid);
+      console.log(this.props.order.user_id);
       let res = await axios.post(
-        'https://drp38-backend.herokuapp.com/notifications/notify/user/', 
+        "https://drp38-backend.herokuapp.com/notifications/notify/user/",
         {
           title: "Order ready to pick up!",
-          body: "Your order from "+restaurantName+" is ready to pick up!",
-          user_id: this.props.uid
-        });
-        console.log(res);
+          body: "Your order from " + restaurantName + " is ready to pick up!",
+          user_id: this.props.order.user_id,
+        }
+      );
+      console.log(res);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   };
 
   /* Cancels order and deletes from the list. */
   onHandleCancel = async () => {
     try {
-      let res = await axios.post('https://drp38-backend.herokuapp.com/orders/change_status/cancelled/' + this.props.id);
+      let res = await axios.post(
+        "https://drp38-backend.herokuapp.com/orders/change_status/cancelled/" +
+          this.props.order._id
+      );
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   };
 
-  printItems() {
-    let splitText = this.props.items.split("\nSpecial requirements");
-    return (
-      <Text style={this.state.isActive ? [styles.content, styles.active] : {height:0}}>
-        <Text style={this.state.isActive ? [styles.content, styles.active] : {height:0}, splitText[1] == undefined ? {fontSize:0} : {color:'red'}}>
-          {"Special requirements" + splitText[1] + "\n\n"}
+  printRequirements() {
+    if (this.props.order.hasOwnProperty("special_request")) {
+      return (
+        <Text
+          style={[
+            this.state.isActive
+              ? [styles.content, styles.active]
+              : { height: 0 },
+            { color: "red" },
+          ]}
+        >
+          {"Special requirements: " + this.props.order.special_request + "\n\n"}
         </Text>
-        {splitText[0]}
-      </Text>
-    );
-  };
+      );
+    }
+  }
 
   render() {
     return (
       <TouchableOpacity
         style={{ borderRadius: 10, margin: 10 }}
-        onPress={this.changeText} >
-         <View>
-        <View style={styles.row}>
-          <View style={styles.leftContainer}>
-            <Button
-              title='Cancel'
-              color='#fa8072'
-              onPress={this.onHandleCancel}>
-            </Button>
-          </View>
+        onPress={this.changeText}
+      >
+        <View>
+          <View style={styles.row}>
+            <View style={styles.leftContainer}>
+              <Button
+                title="Cancel"
+                color="#fa8072"
+                onPress={this.onHandleCancel}
+              ></Button>
+            </View>
 
-          <View style={{ flexDirection: 'column' }}>
-            <Text style={styles.title}>#{this.props.id}</Text>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={[styles.viewItems]}>{this.state.isActive ?  'Hide Items': 'View Items'}</Text>
-              <Icon name={this.state.isActive ? 'keyboard-arrow-up': 'keyboard-arrow-down' } size={30} color={'black'} />
+            <View style={{ flexDirection: "column" }}>
+              <Text style={styles.title}>#{this.props.order._id}</Text>
+              <View style={{ flexDirection: "row" }}>
+                <Text style={[styles.viewItems]}>
+                  {this.state.isActive ? "Hide Items" : "View Items"}
+                </Text>
+                <Icon
+                  name={
+                    this.state.isActive
+                      ? "keyboard-arrow-up"
+                      : "keyboard-arrow-down"
+                  }
+                  size={30}
+                  color={"black"}
+                />
+              </View>
+            </View>
+
+            <View style={styles.rightContainer}>
+              <Button title="Prepared" onPress={this.onHandleReady}></Button>
             </View>
           </View>
 
-          <View style={styles.rightContainer}>
-            <Button
-              title='Prepared'
-              onPress={this.onHandleReady}>
-            </Button>
+          <View>
+            <Text
+              style={
+                this.state.isActive
+                  ? [styles.content, styles.active]
+                  : { height: 0 }
+              }
+            >
+              {this.printRequirements()}
+              <Text>{this.props.order.items}</Text>
+            </Text>
+
+            <View style={styles.inactive}></View>
           </View>
+          <View
+            style={this.state.isActive ? styles.active : styles.inactive}
+          ></View>
         </View>
-        
-        
-        
-        <View>
-        {this.printItems()}
-
-        <View style={styles.inactive}>
-        </View>
-      </View>
-      <View style={this.state.isActive ? styles.active : styles.inactive}></View>
-
-
-      </View>
-      </ TouchableOpacity>
+      </TouchableOpacity>
     );
   }
 }
